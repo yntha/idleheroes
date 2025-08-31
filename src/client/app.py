edf3dcbbca9adfb5a8fd5f9712f660bc6c4b9d49
yntha <126660548+yntha@users.noreply.github.com>
2025-08-30 21:00:20 -0400

    refactor(app): remove unused import from app.py

--- src/client/app.py
+++ src/client/app.py
@@ -1,7 +1,6 @@
 import asyncio
 
 from client.ihnet import IHNetClient
-from client.protobuf.dr2_login_pb_pb2 import pbrsp_echo
 
 
 async def main():
@@ -14,6 +13,7 @@ async def main():
             print(f"Echo response {i+1}: echo={echo.echo}")
     finally:
         await client.disconnect()
+
 
 def cli_entry():
     asyncio.run(main())
