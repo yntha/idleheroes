f457d2e9fb5322b0d8c188ebf9440053653cf9b3
yntha <126660548+yntha@users.noreply.github.com>
2025-09-04 17:55:40 -0400

    feat(app): enhance account authentication flow with debug options and result handling

--- src/client/app.py
+++ src/client/app.py
@@ -1,12 +1,28 @@
 import asyncio
 
+from typing import Any
+
 from google.protobuf.json_format import MessageToJson
 from client.ihnet import IHNetClient
+
+
+# enable debug output
+DEBUG = False
+
+
+def print_result(message: Any):
+    if message.status == 0:
+        print(" Success!")
+    else:
+        print(f" Failed! (status={message.status})")
 
 
 async def main():
     client = await IHNetClient.create_from_config()
     account = client.get_account()
+    config = client.client_config
+
+    config.debug = DEBUG
 
     if not account.account or not account.password:
         print("Account details are incomplete or missing. Please set up your account:")
@@ -25,22 +41,38 @@ async def main():
         # reg = await client.reg()
         # print(f"Registration response: {MessageToJson(reg)}")
 
-        print("\nRequesting salt...")
+        print("Requesting salt...", end="")
         salt = await client.salt()
-        print(f"Salt response: {MessageToJson(salt)}")
 
-        print("\nLogging in...")
-        login = await client.login(salt.salt)
-        print(f"Login response: {MessageToJson(login)}")
-
-        print("\nAuthenticating...")
-        auth = await client.auth(login.uid, login.session)
-        print(f"Auth response: {MessageToJson(auth)}")
-
-        if auth.status == 0:
-            print("\nAuthentication successful!")
+        if config.debug:
+            print(f"\nSalt response: {MessageToJson(salt)}")
         else:
-            print("\nAuthentication failed!")
+            print_result(salt)
+
+        if salt.status != 0:
+            return
+
+        print("Logging in...", end="")
+        login = await client.login(salt.salt)
+
+        if config.debug:
+            print(f"\nLogin response: {MessageToJson(login)}")
+        else:
+            print_result(login)
+
+        if login.status != 0:
+            return
+
+        print("Authenticating...", end="")
+        auth = await client.auth(login.uid, login.session)
+
+        if config.debug:
+            print(f"\nAuth response: {MessageToJson(auth)}")
+        else:
+            print_result(auth)
+
+        if auth.status != 0:
+            return
 
     finally:
         await client.disconnect()
