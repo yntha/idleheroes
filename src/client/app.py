20fed955d16c9f0056eee71165d3d40c89b903fb
yntha <126660548+yntha@users.noreply.github.com>
2025-09-04 15:23:40 -0400

    feat(app): update salt and login response logging to use MessageToJson for better readability

--- src/client/app.py
+++ src/client/app.py
@@ -1,5 +1,6 @@
 import asyncio
 
+from google.protobuf.json_format import MessageToJson
 from client.ihnet import IHNetClient
 
 
@@ -7,14 +8,23 @@ async def main():
     client = await IHNetClient.create_from_config()
 
     try:
-        print("Sending 5 echo requests...")
-        for i in range(5):
-            echo = await client.echo()
-            print(f"Echo response {i+1}: echo={echo.echo}")
+        # print("Sending 5 echo requests...")
+        # for i in range(5):
+        #     echo = await client.echo()
+        #     print(f"Echo response {i+1}: {MessageToJson(echo)}")
+
+        # might not be a good idea to spam registration requests
+        # print("\nRegistering guest account...")
+        # reg = await client.reg()
+        # print(f"Registration response: {MessageToJson(reg)}")
 
         print("\nRequesting salt...")
         salt = await client.salt()
-        print(f"Salt response: status={salt.status} salt='{salt.salt}' uid={salt.uid} flag={salt.flag} cd={salt.cd}")
+        print(f"Salt response: {MessageToJson(salt)}")
+
+        print("\nLogging in...")
+        login = await client.login(salt.salt)
+        print(f"Login response: {MessageToJson(login)}")
     finally:
         await client.disconnect()
 
