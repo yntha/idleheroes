060f1d6abd55f7c3fc66a8bb5a0b0136a7b3bb36
yntha <126660548+yntha@users.noreply.github.com>
2025-09-19 19:07:14 -0400

    refacor(bag): load items directly from data files extracted from game's source code

--- src/client/models/bag.py
+++ src/client/models/bag.py
@@ -3,15 +3,18 @@ from __future__ import annotations
 from dataclasses import dataclass
 from typing import TYPE_CHECKING
 
-from client.models.item_types import ItemType
-
 if TYPE_CHECKING:
     from client.protobuf.dr2_logic_pb_pb2 import pbrsp_sync
 
 
+GOLD_ITEM_ID = 1
+GEM_ITEM_ID = 2
+EXP_ITEM_ID = 3
+
+
 @dataclass
 class IHBagItem:
-    item_id: ItemType
+    item_id: int
     count: int
 
 
@@ -28,24 +31,24 @@ class IHPlayerBag:
 
     @classmethod
     def from_sync(cls, sync: pbrsp_sync) -> IHPlayerBag:
-        items = [IHBagItem(item_id=ItemType(it.id), count=it.num) for it in sync.bag.items]
+        items = [IHBagItem(item_id=it.id, count=it.num) for it in sync.bag.items]
         equipment = [IHBagEquipment(item_id=eq.id, num=eq.num) for eq in sync.bag.equips]
         return cls(items=items, equipment=equipment)
 
-    def get_item_by_type(self, item_type: ItemType) -> IHBagItem | None:
+    def get_item_by_type(self, item_type: int) -> IHBagItem | None:
         for item in self.items:
             if item.item_id == item_type:
                 return item
         return None
 
     def get_gold(self) -> int:
-        gold_item = self.get_item_by_type(ItemType.GOLD)
+        gold_item = self.get_item_by_type(GOLD_ITEM_ID)
         return gold_item.count if gold_item else 0
 
     def get_gems(self) -> int:
-        gem_item = self.get_item_by_type(ItemType.GEM)
+        gem_item = self.get_item_by_type(GEM_ITEM_ID)
         return gem_item.count if gem_item else 0
 
     def get_player_xp(self) -> int:
-        xp_item = self.get_item_by_type(ItemType.EXP)
+        xp_item = self.get_item_by_type(EXP_ITEM_ID)
         return xp_item.count if xp_item else 0
